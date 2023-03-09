1cd480a93f1aff421ec287455a7f5cd6299dd322
Augusto Lopez Brenot <augustolopezbrenot@gmail.com>
2023-03-09 09:39:10 -0300

    CRUD-AppInicio-Docentes

--- AppInicio/urls.py
+++ AppInicio/urls.py
@@ -2,12 +2,18 @@
 
 from django.urls import path
 from AppInicio import views
+from .views import *
 
 urlpatterns = [
     path('', views.Inicio, name='inicio' ),
-    path('docentes/', views.Docentes, name='docentes'),
-    path('alumnos/', views.Alumnos, name='alumnos'),
-    path('practicas/', views.Practicas, name='practicas'),
-    path('busqueda-clase/', views.busqueda_clase, name='busqueda-clase'),
-    path('buscar/', views.buscar, name='buscar')
+    # Urls del modelo Docente
+    path('docentes-list/', DocentesLista.as_view(), name='docentes-list'),
+    path('docentes-detalle/<pk>', DocentesDetail.as_view(), name='docentes-detalle'),
+    path('docentes-nuevo/', DocentesCreate.as_view(), name='docentes-nuevo'),
+    path('docentes-editar/<pk>', DocentesUpdate.as_view(), name='docentes-editar'),
+    path('docentes-eliminar/<pk>', DocentesDelete.as_view(), name='docentes-eliminar'),
+    # path('alumnos/', views.Alumnos, name='alumnos'),
+    # path('practicas/', views.Practicas, name='practicas'),
+    # path('busqueda-clase/', views.busqueda_clase, name='busqueda-clase'),
+    # path('buscar/', views.buscar, name='buscar')
 ]
--- AppInicio/views.py
+++ AppInicio/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 from AppInicio.models import *
+
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 
@@ -25,12 +31,29 @@ def buscar(request):
 
     return HttpResponse (respuesta)
 
-# Views de las demás categorías
-def Docentes(request):
-    return render(request,'AppInicio/docentes.html')
+# Views CRUD Docentes: (Vistas basadas en clases)
 
-def Alumnos(request):
-    return render(request,'AppInicio/alumnos.html')
+class DocentesLista(ListView):
+    model = Docente
+    template_name = 'AppInicio/docentes-list.html'
 
-def Practicas(request):
-    return render(request,'AppInicio/practicas.html')
+class DocentesDetail(DetailView):
+    model = Docente
+    template_name ='AppInicio/docentes-detalle.html'
+
+class DocentesCreate(CreateView):
+    model = Docente
+    template_name = 'AppInicio/docentes-nuevo.html'
+    success_url = reverse_lazy('inicio')
+    fields = ['nombre', 'apellido', 'materia', 'mail', 'DNI']
+
+class DocentesUpdate(UpdateView):
+    model = Docente
+    template_name = 'AppInicio/docentes-nuevo.html'
+    success_url = reverse_lazy('inicio')
+    fields = ['nombre', 'apellido', 'materia', 'mail', 'DNI']
+
+class DocentesDelete(DeleteView):
+    model = Docente
+    template_name = 'AppInicio/docentes-eliminar.html'
+    success_url = reverse_lazy('inicio')
